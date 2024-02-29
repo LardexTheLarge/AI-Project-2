e449ae064399d92c9ea38cca3147edef573e0ae3
LardexTheLarge <0gabevee0@gmail.com>
2024-02-29 15:41:26 -0700

    Added error handling for ml_pipline function and feature_encoding function

--- project_libs.py
+++ project_libs.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import cross_val_score
 import project_libs as libs
@@ -105,31 +105,50 @@ def split_data(data, target_column, split_size=0.2, random_state=42):
         return None, None, None, None
 #----------------------------------------------------------------------------------------------------------------------------------------
 
-def Feature_Encoding(df, target_column, features=None, encoding_method='one_hot'):
-    
+def Feature_Encoding(df, features=None, encoding_method='one_hot'):
     """
-    Hot encoding or label encoding for specified features.
+    Apply feature encoding (one-hot encoding or label encoding) to specified features in a DataFrame.
+
+    Parameters:
+    - df: DataFrame containing the data
+    - features: List of feature column names to encode
+    - encoding_method: Method for encoding categorical features ('one_hot' or 'label')
+
+    Returns:
+    - DataFrame with encoded features
     """
-    if features is None:
-        raise ValueError("List of features must be provided.")
-    if encoding_method not in ['one_hot', 'label']:
-        raise ValueError("Invalid encoding method. Please choose 'one_hot' or 'label'.")
-    if encoding_method == 'one_hot':
-        encoder = OneHotEncoder()
+    try:
+        # Check if df is provided and is a DataFrame
+        if df is None or not isinstance(df, pd.DataFrame):
+            raise ValueError("Please provide a valid DataFrame.")
         
-        for col in features:
-            # Fit and transform on training data
-            #encoder.fit(x_train[col])
-            df[col]= encoder.fit_transform(df[col])
-            # Transform on test data
-            df[col] = encoder.fit_transform(df[col])
-    else:
-        encoder = LabelEncoder()
-        for col in features:
-            #encoder.fit(x_train[col])
-            df[col] = encoder.fit_transform(df[col])
-            df[col] = encoder.fit_transform(df[col])
-    return df
+        # Check if features list is provided
+        if features is None or not isinstance(features, list) or len(features) == 0:
+            raise ValueError("List of features must be provided.")
+
+        # Check if encoding method is valid
+        if encoding_method not in ['one_hot', 'label']:
+            raise ValueError("Invalid encoding method. Please choose 'one_hot' or 'label'.")
+
+        if encoding_method == 'one_hot':
+            encoder = OneHotEncoder()
+            for col in features:
+                # Fit and transform on training data
+                # encoder.fit(x_train[col])
+                df[col] = encoder.fit_transform(df[col])
+                # Transform on test data
+                df[col] = encoder.fit_transform(df[col])
+        else:
+            encoder = LabelEncoder()
+            for col in features:
+                # encoder.fit(x_train[col])
+                df[col] = encoder.fit_transform(df[col])
+                df[col] = encoder.fit_transform(df[col])
+
+        return df
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 #----------------------------------------------------------------------------------------------------------
 def plot_correlation(data):
@@ -171,68 +190,120 @@ def create_pair_plot(data):
     plt.show()
 
 #--------------------------------------------------------------------------------------------------------------------
-def run_ml_pipeline(df, target_column, features, models,encoding_method='label',  use_cross_validation=True):
+def run_ml_pipeline(df, target_column, features, models, encoding_method='label', use_cross_validation=True):
     """
     Run a pipeline of machine learning models on preprocessed data.
 
     Parameters:
-    - X_train: Preprocessed training features
-    - X_test: Preprocessed test features
-    - y_train: Training target variable
-    - y_test: Test target variable
+    - df: DataFrame containing the data
+    - target_column: Name of the target column
+    - features: List of feature column names
     - models: List of tuples (model_name, model_instance, model_parameters)
+    - encoding_method: Method for encoding categorical features
     - use_cross_validation: Whether to use cross-validation or model scoring
     """
-    df = preprocess_missing_values(df)
-    print('_'*100)
-    df = Feature_Encoding(df = df, target_column = target_column, features=features, encoding_method = encoding_method)
-    print('_'*100)
-    #TODO: scale the split data
-    
-    X_train, X_test, y_train, y_test = split_data(df, target_column = target_column, split_size=0.2, random_state=42)
-    print('_'*100)
-    df = libs.preprocess_missing_values(df)
-    print('_'*100)
+    try:
+        # Check if df is provided
+        if df is None or not isinstance(df, pd.DataFrame):
+            raise ValueError("Please provide a valid DataFrame.")
+        
+                # Check if features list is provided
+        if features is None or not isinstance(features, list) or len(features) == 0:
+            raise ValueError("Please provide a valid list of feature column names.")
 
-    score_test = []
-    score_training = []
-    model_names = []
+        # Check if target column is provided
+        if target_column is None or not isinstance(target_column, str) or target_column not in df.columns:
+            raise ValueError("Please provide a valid target column name that exists in the DataFrame.")
 
-    for model_name, model, params in models:
-        # Create a pipeline for each model
-        pipeline = Pipeline([
-            ('model', model(**params))
-        ])
+        # Check if models list is provided
+        if models is None or not isinstance(models, list) or len(models) == 0:
+            raise ValueError("Please provide a valid list of models.")
+        
+        # Validate the format of each tuple in the models list
+        for model_tuple in models:
+            if not isinstance(model_tuple, tuple) or len(model_tuple) != 3:
+                raise ValueError("Each model tuple must have three elements: (model_name, model_instance, model_parameters).")
+            model_name, model_instance, model_params = model_tuple
+            if not isinstance(model_name, str) or not hasattr(model_instance, '__call__') or not isinstance(model_params, dict):
+                raise ValueError("Invalid format for model tuple. Example format: ('Random Forest', RandomForestRegressor, {'n_estimators': 100}).")
 
-        if use_cross_validation:
-            # Evaluate the model using cross-validation
-            scores = cross_val_score(pipeline, X_train, y_train, cv=5)
-            mean_score = scores.mean()
-            std_score = scores.std()
-            print(f"Model: {model_name}")
-            print(f"Mean Accuracy: {mean_score:.2f}")
-            print(f"Standard Deviation: {std_score}")
-            print("-" * 40)
-            score_training.append(mean_score)
-            score_test.append(np.nan)  # Cross-validation doesn't provide test scores
-        else:
-            # Fit the model and compute scores on training and test sets
-            train_score = pipeline.fit(X_train, y_train).score(X_train, y_train)
-            test_score = pipeline.score(X_test, y_test)
-            print(f"Model: {model_name}")
-            print(f"Training Score: {train_score:.2f}")
-            print(f"Test Score: {test_score:.2f}")
-            print("-" * 40)
-            score_training.append(train_score)
-            score_test.append(test_score)
+        # Preprocessing
+        print('_'*100)
+        df = preprocess_missing_values(df)
+        print('_'*100)
+        df = Feature_Encoding(df=df, features=features, encoding_method=encoding_method)
+        print('_'*100)
+        X_train, X_test, y_train, y_test = split_data(df, target_column=target_column, split_size=0.2, random_state=42)
+        print('_'*100)
+        x_scaler = StandardScaler().fit(X_train)
+        X_train_scaled = x_scaler.transform(X_train)
+        X_test_scaled = x_scaler.transform(X_test)
 
-        model_names.append(model_name)
+        # Model evaluation
+        score_test = []
+        score_training = []
+        model_names = []
 
-    result = pd.DataFrame({'Model Name': model_names, 'Training Score': score_training, 'Test Score': score_test})
-    return X_train, X_test, y_train, y_test,result
+        for model_name, model, params in models:
+            # Create a pipeline for each model
+            pipeline = Pipeline([
+                ('model', model(**params))
+            ])
+
+            if use_cross_validation:
+                # Evaluate the model using cross-validation
+                scores = cross_val_score(pipeline, X_train_scaled, y_train, cv=5)
+                mean_score = scores.mean()
+                std_score = scores.std()
+                print(f"Model: {model_name}")
+                print(f"Mean Accuracy: {mean_score:.2f}")
+                print(f"Standard Deviation: {std_score}")
+                print("-" * 40)
+                score_training.append(mean_score)
+                score_test.append(np.nan)  # Cross-validation doesn't provide test scores
+            else:
+                # Fit the model and compute scores on training and test sets
+                train_score = pipeline.fit(X_train_scaled, y_train).score(X_train_scaled, y_train)
+                test_score = pipeline.score(X_test_scaled, y_test)
+                print(f"Model: {model_name}")
+                print(f"Training Score: {train_score:.2f}")
+                print(f"Test Score: {test_score:.2f}")
+                print("-" * 40)
+                score_training.append(train_score)
+                score_test.append(test_score)
+
+            model_names.append(model_name)
+
+        result = pd.DataFrame({'Model Name': model_names, 'Training Score': score_training, 'Test Score': score_test})
+        return X_train_scaled, X_test_scaled, y_train, y_test, result
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 #--------------------------------------------------------------------------------------------------------------------
 def hyperparameter_tuning(model, param_grid, X_train, y_train, scoring):
-    random_search = RandomizedSearchCV(estimator=model, param_distributions=param_grid, scoring=scoring, n_iter=100, cv=5)
+    """
+    Perform hyperparameter tuning using Randomized Search CV on a given model.
+
+    Parameters:
+    - model: Estimator object (model to be tuned)
+    - param_grid: Dictionary of hyperparameter distributions to search
+    - X_train: Training features
+    - y_train: Training target variable
+    - scoring: Scoring metric to optimize during tuning
+
+    Returns:
+    - best_model: Best estimator model after tuning
+    - best_params: Best parameters found during tuning
+    - best_score: Best score achieved during tuning
+    """
+    # Perform Randomized Search CV
+    random_search = RandomizedSearchCV(estimator=model, param_distributions=param_grid, scoring=scoring, n_iter=100, cv=5, random_state=42)
     random_search.fit(X_train, y_train)
-    return random_search.best_estimator_, random_search.best_params_, random_search.best_score_
+
+    # Get the best parameters and model
+    best_params = random_search.best_params_
+    best_model = random_search.best_estimator_
+    best_score = random_search.best_score_
+
+    return best_model, best_params, best_score
